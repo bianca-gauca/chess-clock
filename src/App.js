79287fbf7af8632d1fa91befd3f6c53026ad1559
Gauca <bianca.gauca@haufe.com>
2023-02-11 15:27:32 +0200

    feat(chess): new version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,248 @@
-import { useState } from 'react';
+import React from 'react';
+import { Dialog, IconButton, Toolbar, Typography, Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import CloseIcon from '@mui/icons-material/Close';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Counter from './Counter/Counter';
+import ReactConfetti from 'react-confetti';
+import { RestartAlt } from '@mui/icons-material';
+import { KeepAwake } from '@capacitor-community/keep-awake';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)'
+  },
+}));
+
+const INITIAL_TIME = {
+  min: 15,
+  sec: 0
+};
 
 function App() {
-  const [counter1Disabled, setCounter1Disabled] = useState(true);
-  const [counter2Disabled, setCounter2Disabled] = useState(true);
+  const [player1, setPlayer1] = useState({
+    name: 'Alex',
+    disabled: true,
+    time: {
+      ...INITIAL_TIME
+    },
+    moves: 0
+  });
+  const [player2, setPlayer2] = useState({
+    name: 'Bianca',
+    disabled: true,
+    time: {
+      ...INITIAL_TIME
+    },
+    moves: 0
+  });
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [winner, setWinner] = useState();
+  const { innerWidth: windowWidth } = window;
 
-  const handleClickCounter1 = () => {
-    setCounter1Disabled(true);
-    setCounter2Disabled(false);
+  useEffect(() => {
+    const keepScreenAwake = async () => {
+      if (!player1.disabled || !player2.disabled) {
+        await KeepAwake.keepAwake();
+      } else {
+        await KeepAwake.allowSleep();
+      }
+    }
+
+    keepScreenAwake().catch(() => console.log('Could not keep the screen awake'));
+  }, [player1.disabled, player2.disabled])
+
+  const handleCounter1Click = () => {
+    const {disabled, moves} = player1;
+
+    setPlayer1({
+      ...player1,
+      disabled: true,
+      moves: disabled ? moves : moves + 1
+    });
+    setPlayer2({
+      ...player2,
+      disabled: false
+    });
   }
 
-  const handleClickCounter2 = () => {
-    setCounter1Disabled(false);
-    setCounter2Disabled(true);
+  const handleCounter2Click = () => {
+    const {disabled, moves} = player2;
+
+    setPlayer1({
+      ...player1,
+      disabled: false
+    });
+    setPlayer2({
+      ...player2,
+      disabled: true,
+      moves: disabled ? moves : moves + 1
+    });
+  }
+
+  const handleCounter1Change = ({time}) => {
+    setPlayer1({
+      ...player1,
+      time,
+    });
+  }
+
+  const handleCounter2Change = ({time}) => {
+    setPlayer2({
+      ...player2,
+      time,
+    });
+  }
+
+  const handleCounterFinish = ({name: playerName}) => {
+    setPlayer1({
+      ...player1,
+      disabled: true
+    });
+    setPlayer2({
+      ...player2,
+      disabled: true
+    });
+    setWinner(playerName);
+    setDialogOpen(true);
+  }
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  }
+
+  const resetCounters = () => {
+    setPlayer1({
+      ...player1,
+      time: {
+        ...INITIAL_TIME
+      },
+      disabled: true,
+      moves: 0
+    });
+    setPlayer2({
+      ...player2,
+      time: {
+        ...INITIAL_TIME
+      },
+      disabled: true,
+      moves: 0
+    });
+  }
+
+  const handleDialogReset = () => {
+    resetCounters();
+    handleCloseDialog();
   }
 
   return (
     <div>
-      <Counter minutes={1} disabled={counter1Disabled} onClick={handleClickCounter1}></Counter>
-      
-      <Counter disabled={counter2Disabled} onClick={handleClickCounter2}></Counter>
+      <div className='counter1'>
+        <Counter
+          player={player1}
+          onClick={handleCounter1Click}
+          onChange={handleCounter1Change}
+          onFinish={() => handleCounterFinish(player1)}
+        >
+        </Counter>
+      </div>
+
+       <Box
+          sx={{
+              position: 'relative',
+              width: '100%'
+          }}
+          onClick={handleDialogReset}
+        >
+          <RestartAlt sx={{
+            position: 'absolute',
+            color: '#2c2c3b',
+            width: '50px',
+            height: '50px',
+            cursor: 'pointer',
+            left: '50%',
+            ml: '-25px',
+            top: '40px',
+          }}/>
+        </Box>
+
+      <div className='counter2'>
+        <Counter
+          player={player2}
+          onClick={handleCounter2Click}
+          onChange={handleCounter2Change}
+          onFinish={() => handleCounterFinish(player2)}
+        >
+        </Counter>
+      </div>
+
+      <BootstrapDialog
+        fullScreen
+        open={isDialogOpen}
+        onClose={handleCloseDialog}
+        sx={{
+          background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)'
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            onClick={handleCloseDialog}
+            aria-label="close"
+            sx={{
+              position: 'absolute',
+              right: 20,
+              top: 20,
+              color: '#373737'
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+        <Box>
+          <ReactConfetti
+            width={windowWidth}
+            height={'300px'}
+            numberOfPieces={60}
+            opacity={0.7}
+          />
+        </Box>
+
+        <Box sx={{
+            textAlign: 'center',
+            mt: '20%',
+            width: '100%',
+          }}>
+          <Typography variant='h4' sx={{
+            color: '#373737'
+          }}>
+              And the winner is
+          </Typography>
+          <Typography variant='h2' sx={{
+            color: '#ffffff'
+          }}>
+              {winner}
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+              color: '#ffffff',
+              position: 'absolute',
+              bottom: '100px',
+              left: '50%',
+              ml: '-40px'
+          }}
+          onClick={handleDialogReset}
+        >
+          <RestartAlt sx={{
+            width: '80px',
+            height: '80px',
+            cursor: 'pointer'
+          }}/>
+        </Box>
+      </BootstrapDialog>
     </div>
   );
 }
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,41 +1,67 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect } from 'react'
 
 import './Counter.css';
 
-export default function Counter({minutes = 15, disabled = false, onClick}) {
-  console.log('rerender')
-  const [{min, sec}, setTime] = useState({
-    min: minutes,
-    sec: 0
-  });
+export default function Counter({
+  player = {
+    time: {
+      min: 15,
+      sec: 0
+    },
+    name: 'Player',
+    disabled: false, 
+  }, 
+  onClick,
+  onFinish,
+  onChange
+}) {
+  const { time, name: playerName, disabled, moves } = player;
+  const {min, sec} = time;
 
-  useEffect(() => {
-    const timerId = setInterval(() => decrementTimer(), 1000);
-
-    return () => clearInterval(timerId);
-  });
-
-  const decrementTimer = () => {
-    if ((min > 0 || sec > 0) && disabled === false) {
+  const decrementTimer = useCallback(() => {
+    if ((min > 0 || sec > 0) && !disabled) {
+      let newMin
+      let newSec
       if (sec === 0) {
-        setTime({
-          min: min - 1,
-          sec: 59
-        })
+        newMin = min - 1;
+        newSec = 59;
       } else {
-        setTime({
-          min,
-          sec: sec - 1
-        })
+        newMin = min;
+        newSec = sec - 1;
+      }
+
+      onChange && onChange({
+        time: {
+          min: newMin,
+          sec: newSec
+        },
+      })
+
+      if (newMin === 0 && newSec === 0) {
+        onFinish && onFinish();
       }
     }
-  };
+  }, [min, sec, disabled, onChange, onFinish]);
+
+  useEffect(() => {
+    const intervalId = setTimeout(() => decrementTimer(), 1000);
+
+    return () => clearTimeout(intervalId);
+  }, [decrementTimer]);
+
+  const numberToTime = (value) => value < 9 ? `0${value}` : value;
+  const activeClass = disabled ? '' : 'active';
 
   return (
-    <div className='counter' onClick={onClick}>
-      <div className='min' > {min}</div> 
-      <div className='points'>:</div> 
-      <div className='sec'>{sec}</div>
+    <div className={`counter-container ${activeClass}`} onClick={onClick}>
+      <div className='counter-player'>{playerName}</div>
+      <div className='counter-time'>
+        <div className='min'> {numberToTime(min)}</div> 
+        <div className='points'>:</div> 
+        <div className='sec'>{numberToTime(sec)}</div>
+      </div>
+      <div className='counter-moves'>Moves: {moves}</div>
     </div>
+    
   );
 }
